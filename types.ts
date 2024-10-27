29f3b9c1c07739e1e156f2859a0f30f4a0be024c
Chytoe <tochyegeonu97@gmail.com>
2024-10-27 09:13:52 -0500

    Initial commit

---
+++ types.ts
@@ -0,0 +1,22 @@
+
+export type Day = {
+    index?: number,
+    day?: string
+};
+
+export type Week = {
+    index?: number,
+    days: Array<Day>
+};
+
+export type Month = {
+    weeks: Array<Week>,
+    month: string,
+    index: number,
+    year: number,
+    isLeap: boolean,
+};
+
+export type Past = Array<Month>
+
+export type PastPromise = Promise<Past>
